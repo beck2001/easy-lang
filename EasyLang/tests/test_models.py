45673adb027dd7426ed285c1d19970a483f73757
beck2001 <muratbek.sharibekoff@gmail.com>
2023-12-10 19:26:39 +0600

    Added tests

--- EasyLang/tests/test_models.py
+++ EasyLang/tests/test_models.py
@@ -1,6 +1,58 @@
 from django.test import TestCase
 from django.utils import timezone
-from mainApp.models import User, Manager, Project, Translator, Editor, Project_Translator, Project_Editor, Pages_per_day, Approve, Disapprove
+from django.db import IntegrityError
+from mainApp.models import User, Manager, Project, Translator, Editor, Project_Translator, Project_Editor, Pages_per_day, Approve, Disapprove, UserManager
+from datetime import date
+
+
+class CreateUserTestCase(TestCase):
+
+    def test_create_user_success(self):
+        user = User.objects.create_user(tg_id=100, name="John", surname="Doe", password="password123")
+        self.assertIsNotNone(user)
+        self.assertEqual(user.name, "John")
+        self.assertEqual(user.surname, "Doe")
+        self.assertEqual(user.tg_id, 100)
+        self.assertTrue(user.check_password("password123"))
+
+    def test_create_user_no_tg_id(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(tg_id=None, name="John", surname="Doe", password="password123")
+
+    def test_create_user_no_name(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(tg_id=101, name=None, surname="Doe", password="password123")
+
+    def test_create_user_no_surname(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(tg_id=102, name="John", surname=None, password="password123")
+
+    def test_create_user_no_password(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(tg_id=103, name="John", surname="Doe", password=None)
+
+
+class CreateSuperuserTestCase(TestCase):
+
+    def test_create_superuser_success(self):
+        superuser = User.objects.create_superuser(tg_id=200, name="Admin", surname="User", password="adminpass")
+        self.assertIsNotNone(superuser)
+        self.assertEqual(superuser.name, "Admin")
+        self.assertEqual(superuser.surname, "User")
+        self.assertEqual(superuser.tg_id, 200)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.check_password("adminpass"))
+
+    def test_create_superuser_no_is_staff(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(tg_id=201, name="Admin", surname="User", password="adminpass", is_staff=False)
+
+    def test_create_superuser_no_is_superuser(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(tg_id=202, name="Admin", surname="User", password="adminpass", is_superuser=False)
+
 
 class ModelTestCase(TestCase):
     def setUp(self):
@@ -37,3 +89,24 @@ class ModelTestCase(TestCase):
         pages_per_day = Pages_per_day.objects.create(translator=self.translator, project=self.project, pages_count=10)
         self.assertEqual(str(pages_per_day), 'John Doe Test Project 10')
 
+    def test_approve_creation(self):
+        pages_per_day = Pages_per_day.objects.create(translator=self.translator, project=self.project, pages_count=10)
+        approve = Approve.objects.create(
+            pages_per_day=pages_per_day,
+            editor=self.editor,
+            comment="Approved"
+        )
+        self.assertEqual(approve.pages_per_day, pages_per_day)
+        self.assertEqual(approve.editor, self.editor)
+        self.assertEqual(approve.comment, "Approved")
+
+    def test_disapprove_creation(self):
+        pages_per_day = Pages_per_day.objects.create(translator=self.translator, project=self.project, pages_count=10)
+        disapprove = Disapprove.objects.create(
+            pages_per_day=pages_per_day,
+            editor=self.editor,
+            comment="Disapproved"
+        )
+        self.assertEqual(disapprove.pages_per_day, pages_per_day)
+        self.assertEqual(disapprove.editor, self.editor)
+        self.assertEqual(disapprove.comment, "Disapproved")
